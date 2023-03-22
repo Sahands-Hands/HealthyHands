c2e29b8c1312d86f980320b0d3ef5739c1a112f7
ryanmweiss <rmw1075@rmweiss.net>
2023-03-22 15:18:59 -0600

    updated user controller. Also updated program.cs to include roles, and updated route in UserInfo.razor

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 using HealthyHands.Server.Data;
 using HealthyHands.Server.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentityServer()
