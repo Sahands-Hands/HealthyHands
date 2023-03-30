7d8a0e7077cd7b9dbdbf5486ec572e1e7be3e506
mainerg <gmainerv@gmail.com>
2023-03-30 10:14:39 -0600

    User Detail repository is up to date.

--- Client/HttpRepository/UserHttpRepository/IUserHttpRepository.cs
+++ Client/HttpRepository/UserHttpRepository/IUserHttpRepository.cs
@@ -1,9 +1,13 @@
 ﻿using HealthyHands.Shared.Models;
 namespace HealthyHands.Client.HttpRepository.UserRepository
 {
+    /// <summary>
+    /// An interface that contains two asynchronous methods: "GetUserInfo()" which returns UserDto object, and "UpdateUserInfo()" which takes UserDto object and returns no value
+    /// This repository is used to represent a repository for managing user information via HTTP requests
+    /// </summary>
     public interface IUserHttpRepository
     {
         Task<UserDto> GetUserInfo();
-        //Task<UserDto> UpdateUserInfo();
+        Task UpdateUserInfo(UserDto userDto);
     }
 }
--- Client/HttpRepository/UserHttpRepository/UserHttpRepository.cs
+++ Client/HttpRepository/UserHttpRepository/UserHttpRepository.cs
@@ -9,10 +9,24 @@ namespace HealthyHands.Client.HttpRepository.UserRepository
         {
             _httpClient = httpClient;
         }
+        /// <summary>
+        /// This method uses HTTP client to send a GET request to the API and retrieves the data in JSON format and deserialize it.
+        /// </summary>
+        /// <returns>UserDto object</returns>
         public async Task<UserDto> GetUserInfo()
         {
             UserDto user = await _httpClient.GetFromJsonAsync<UserDto>("user");
             return user;
+        }
+        /// <summary>
+        /// A method that uses the HTTP client to send a PUT request to the "user/update", the request body is populated with provided UserDto
+        /// </summary>
+        /// <param name="userDto"></param>
+        /// <returns>returns the response of the updated information provided by the user</returns>
+        public async Task UpdateUserInfo(UserDto userDto)
+        {
+            UserDto userdto = new UserDto();
+            var response = await _httpClient.PutAsJsonAsync<UserDto>("user/update", userDto);
         }
     }
 }
