231bd7e1d43336ded47f96168263675c04062730
Sahand Ghaemi <sahandghaemi98@outlook.com>
2023-04-03 17:13:33 -0600

    Fixed the logic behind a lot of things in the weight

--- Server/Program.cs
+++ Server/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = builder.Configuration.GetConnectionString("RyanConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
