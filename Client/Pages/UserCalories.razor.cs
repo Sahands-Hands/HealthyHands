9fd62fd83cd59cad524ac00fa1caff8f119d83a8
Artur Ayzenberg <aayzenberg13@gmail.com>
2023-04-06 10:17:26 -0600

    save feature implemented and works as expected

--- Client/Pages/UserCalories.razor.cs
+++ Client/Pages/UserCalories.razor.cs
@@ -23,8 +23,10 @@ namespace HealthyHands.Client.Pages
         public UserDto UserInfo { get; set; } = new();
         public string Goal { get; set; } = "maintain";
         public double Calories { get; set; }
+        public int WeightObjective { get; set; }
         public string Message { get; set; } = "Awaiting Weight Goal";
         public string CalorieMessage { get; set; } = "";
+        public string SaveMessage { get; set; } = "";
 
         protected override async Task OnInitializedAsync()
         {
@@ -45,6 +47,27 @@ namespace HealthyHands.Client.Pages
             {
                 NavigationManager.NavigateTo("/defaultusercalories");
             }
+            if((UserInfo.CalorieGoal != null && UserInfo.CalorieGoal != 0) && (UserInfo.WeightGoal != null))
+            {
+                Calories = (double)UserInfo.CalorieGoal;
+                WeightObjective = (int)UserInfo.WeightGoal;
+            }
+
+            if(WeightObjective == 0)
+            {
+                Message = "Your saved weight goal is to lose weight";
+                CalorieMessage = $"Your saved daily calorie intake is {Calories} calories";
+            }
+            else if(WeightObjective == 1)
+            {
+                Message = "Your saved weight goal is to maintain weight";
+                CalorieMessage = $"Your saved daily calorie intake is {Calories} calories";
+            }
+            else if (WeightObjective == 2)
+            {
+                Message = "Your saved weight goal is to gain weight";
+                CalorieMessage = $"Your saved daily calorie intake is {Calories} calories";
+            }
 
         }
 
@@ -62,6 +85,7 @@ namespace HealthyHands.Client.Pages
 
         public void MaintainWeight()
         {
+            WeightObjective = 1;
             Goal = "maintain";
             CalculateCalories();
             Message = $"Your desired goal is to {Goal} weight";
@@ -69,6 +93,7 @@ namespace HealthyHands.Client.Pages
         }
         public void GainWeight()
         {
+            WeightObjective = 2;
             Goal = "gain";
             CalculateCalories();
             Message = $"Your desired goal is to {Goal} weight";
@@ -76,10 +101,47 @@ namespace HealthyHands.Client.Pages
         }
         public void LoseWeight()
         {
+            WeightObjective = 0;
             Goal = "lose";
             CalculateCalories();
             Message = $"Your desired goal is to {Goal} weight";
             CalorieMessage = $"To meet your goal, your recommended daily calorie intake is {Calories}";
+        }
+
+        public async void SaveCalories()
+        {
+            int calorieGoal = (int)(Math.Round(Calories, MidpointRounding.AwayFromZero));
+            var tempUserDto = new UserDto()
+            {
+                Id = UserInfo.Id,
+                UserName = UserInfo.UserName,
+                FirstName = UserInfo.FirstName,
+                LastName = UserInfo.LastName,
+                Height = UserInfo.Height,
+                Gender = UserInfo.Gender,
+                ActivityLevel = UserInfo.ActivityLevel,
+                WeightGoal = WeightObjective,
+                CalorieGoal = calorieGoal,
+                BirthDay = UserInfo.BirthDay,
+                LockoutEnabled = UserInfo.LockoutEnabled,
+                IsAdmin = UserInfo.IsAdmin,
+                UserMeals = UserInfo.UserMeals,
+                UserWeights = UserInfo.UserWeights,
+                UserWorkouts = UserInfo.UserWorkouts
+            };
+            var updated = false;
+            if(Calories != 0)
+            {
+                updated = await UserHttpRepository.UpdateUserInfo(tempUserDto);
+            }
+            if (updated)
+            {
+                SaveMessage = "The calorie goal has been saved succesfully";
+            }
+            else
+            {
+                SaveMessage = "There was trouble updating the user please try again";
+            }
         }
 
         private double CalculateRecommendedCalories(int gender, double weight, int height, int age, string goal)
